9df8fd60b2d0eb060b3a490fae23b335b1da40a6
Javier Goizueta <jgoizueta@gmail.com>
2012-04-18 20:16:37 +0200

    Load rake tasks when used as a Gem.

--- lib/modalfields.rb
+++ lib/modalfields.rb
@@ -1,3 +1,14 @@
 require 'modalfields/modalfields'
 require 'modalfields/standardfields'
 
+if defined?(Rails)
+  if Rails.version.split('.').first.to_i > 2
+    class BackupTask < Rails::Railtie
+      rake_tasks do
+        Dir[File.join(File.dirname(__FILE__), 'tasks', '**/*.rake')].each { |f| load f }
+      end
+    end
+  else
+    Dir[File.join(File.dirname(__FILE__), 'tasks', '**/*.rake')].each { |f| load f }
+  end
+end
